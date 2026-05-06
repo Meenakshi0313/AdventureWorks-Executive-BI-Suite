11a16077fb89dd1a78d678bc22e3977672562273
Meenakshi Singh <meenakshi.singh.ind@gmail.com>
2026-05-06 13:28:51 +0530

    update files

--- Sql-Scripts/01_Gold_Reporting_Views.sql
+++ Sql-Scripts/01_Gold_Reporting_Views.sql
@@ -18,23 +18,45 @@ IF OBJECT_ID('v_DimProduct', 'V') IS NOT NULL
     DROP VIEW v_DimProduct;
 GO
 
-
-CREATE VIEW v_DimProduct AS
+CREATE OR ALTER VIEW v_DimProduct AS
 SELECT 
-    p.ProductKey,
+    p.ProductKey AS [ProductKey],
     p.ProductAlternateKey AS [Product Code],
     p.EnglishProductName AS [Product Name],
     ISNULL(ps.EnglishProductSubcategoryName,'Unknown') AS [Subcategory],
     ISNULL(pc.EnglishProductCategoryName,'Unknown') AS [Category],
-    p.Color,
-    p.Size,
-    p.ModelName AS [Model]
+    p.Color AS [Color],
+    p.Size AS [Size],
+    p.ModelName AS [Model],
+    p.StandardCost AS [Unit Cost],
+    p.ListPrice AS [List Price]  
 FROM DimProduct p
-LEFT JOIN DimProductSubcategory ps 
-    ON p.ProductSubcategoryKey = ps.ProductSubcategoryKey
-LEFT JOIN DimProductCategory pc 
-    ON ps.ProductCategoryKey = pc.ProductCategoryKey;
-    GO
+LEFT JOIN DimProductSubcategory ps ON p.ProductSubcategoryKey = ps.ProductSubcategoryKey
+LEFT JOIN DimProductCategory pc ON ps.ProductCategoryKey = pc.ProductCategoryKey;
+GO
+
+
+-- =============================================================================
+-- Create Dimension: v_DimReseller
+-- =============================================================================
+
+IF OBJECT_ID('v_DimReseller', 'V') IS NOT NULL
+    DROP VIEW v_DimReseller;
+GO
+
+CREATE OR ALTER VIEW v_DimReseller AS
+SELECT 
+    r.ResellerKey AS [ResellerKey],
+    r.ResellerName AS [Reseller Name],
+    r.BusinessType AS [Business Type],
+    r.YearOpened AS [Year Opened],
+    r.NumberEmployees AS [Number Employees],
+    g.City AS [City],
+    g.EnglishCountryRegionName AS [Country]
+FROM DimReseller r
+LEFT JOIN DimGeography g ON r.GeographyKey = g.GeographyKey;
+GO
+
 
 -- =============================================================================
 -- Create Dimension: v_DimCustomer
@@ -46,16 +68,17 @@ GO
 
 CREATE VIEW v_DimCustomer AS
 SELECT
-    c.CustomerKey,
+    c.CustomerKey AS [CustomerKey],
     c.FirstName + ' ' + c.LastName AS [Customer Name],
-    c.Gender,
-    c.YearlyIncome,
+    c.Gender AS [Gender],
+    c.YearlyIncome AS [Yearly Income],
+    DATEDIFF(YEAR, c.BirthDate, GETDATE()) AS [Age],
     c.EnglishEducation AS [Education],
     c.EnglishOccupation AS [Occupation],
-    c.HouseOwnerFlag AS [Home Owner],
-    c.NumberCarsOwned,
-    c.DateFirstPurchase,
-    g.City,
+   CASE WHEN c.HouseOwnerFlag = '1' THEN 'Yes' ELSE 'No' END AS [Home Owner],
+    c.NumberCarsOwned AS [Number Cars Owned],
+    c.DateFirstPurchase AS [First Purchase Date],
+    g.City AS [City],
     g.StateProvinceName AS [State],
     g.EnglishCountryRegionName AS [Country]
 FROM DimCustomer c
@@ -73,9 +96,10 @@ GO
 
 CREATE OR ALTER VIEW v_DimEmployee AS
 SELECT 
-    e.EmployeeKey,
-    e.ParentEmployeeKey,
+    e.EmployeeKey AS [EmployeeKey],
+    ISNULL(CAST(e.ParentEmployeeKey AS VARCHAR), 'Top Level') AS [ParentEmployeeKey],
     e.FirstName + ' ' + e.LastName AS [Employee Name],
+<<<<<<< HEAD:Sql-Scripts/AdventureWork_Gold_Layer_View.sql
     e.Title,
     e.DepartmentName,
     e.HireDate,
@@ -84,11 +108,21 @@ SELECT
     e.MaritalStatus,
     e.BaseRate,
     e.SalesTerritoryKey,
+=======
+    e.Title AS [Title],
+    e.DepartmentName AS [Department],
+    e.HireDate AS [Hire Date],
+    DATEDIFF(YEAR, e.HireDate, GETDATE()) AS [Years of Service],
+    CASE WHEN e.Gender = 'M' THEN 'Male' ELSE 'Female' END AS [Gender],
+    CASE WHEN e.MaritalStatus = 'M' THEN 'Married' ELSE 'Single' END AS  [Marital Status],
+    e.BaseRate AS [Base Rate],
+    e.SalesTerritoryKey AS [SalesTerritoryKey],
+>>>>>>> a18143be2e121dacd996d4ec19789bdfe19761b9:Sql-Scripts/01_Gold_Reporting_Views.sql
     CASE 
         WHEN e.SalesPersonFlag = 1 THEN 'Sales' 
-        ELSE 'Support/Admin' 
+        ELSE 'Support' 
     END AS [Role Category],
-    e.CurrentFlag
+    e.CurrentFlag AS [Current Flag]
 FROM DimEmployee e
 WHERE e.CurrentFlag = 1;
 GO
@@ -103,16 +137,20 @@ GO
 
 CREATE VIEW v_DimDate AS
 SELECT
-    DateKey,
+    DateKey AS [DateKey],
     FullDateAlternateKey AS [Date],
     EnglishDayNameOfWeek AS [Day],
-    DayNumberOfMonth,
+    DayNumberOfMonth AS [Day Number Of Month],
     EnglishMonthName AS [Month],
     MonthNumberOfYear AS [Month Number],
     CalendarQuarter AS [Quarter],
     CalendarYear AS [Year]
 FROM DimDate
+<<<<<<< HEAD:Sql-Scripts/AdventureWork_Gold_Layer_View.sql
 WHERE CalendarYear >= 2010; 
+=======
+WHERE DateKey BETWEEN 20101201 AND 20140131;
+>>>>>>> a18143be2e121dacd996d4ec19789bdfe19761b9:Sql-Scripts/01_Gold_Reporting_Views.sql
 GO
 
 -- =============================================================================
@@ -125,7 +163,7 @@ GO
 
     CREATE VIEW v_DimTerritory AS
 SELECT
-    SalesTerritoryKey,
+    SalesTerritoryKey AS [SalesTerritoryKey],
     SalesTerritoryRegion AS [Region],
     SalesTerritoryCountry AS [Country],
     SalesTerritoryGroup AS [Group]
@@ -142,13 +180,13 @@ GO
 
 CREATE VIEW v_DimPromotion AS
 SELECT
-    PromotionKey,
+    PromotionKey AS [PromotionKey],
     EnglishPromotionName AS [Promotion Name],
     EnglishPromotionType AS [Promotion Type],
     EnglishPromotionCategory AS [Promotion Category],
     DiscountPct AS [Discount Percent],
-    StartDate,
-    EndDate
+    StartDate AS [Start Date],
+    EndDate AS [End Date]
 FROM DimPromotion;
 GO
 
@@ -162,6 +200,7 @@ GO
 
 CREATE VIEW v_FactProductInventory AS
 SELECT 
+<<<<<<< HEAD:Sql-Scripts/AdventureWork_Gold_Layer_View.sql
     ProductKey,
     DateKey,
     MovementDate,
@@ -170,6 +209,17 @@ SELECT
     UnitsOut,
     UnitsBalance
 FROM FactProductInventory;
+=======
+    ProductKey AS [ProductKey],
+    DateKey AS [DateKey],
+    MovementDate AS [Movement Date],
+    UnitCost AS [Unit Cost],
+    UnitsIn AS [Units In],
+    UnitsOut AS [Units Out],
+    UnitsBalance AS [Units Balance]
+FROM FactProductInventory
+WHERE DateKey BETWEEN 20101201 AND 20140131;;
+>>>>>>> a18143be2e121dacd996d4ec19789bdfe19761b9:Sql-Scripts/01_Gold_Reporting_Views.sql
 GO
 
 -- =============================================================================
@@ -186,11 +236,18 @@ SELECT
         (DateKey / 10000), 
         (DateKey % 10000) / 100, 
         (DateKey % 100)
+<<<<<<< HEAD:Sql-Scripts/AdventureWork_Gold_Layer_View.sql
     ) AS ActualDate,
     EmployeeKey, 
     SalesAmountQuota
 FROM FactSalesQuota
 WHERE DateKey >= 20100101;
+=======
+    ) AS [Actual Date],
+    EmployeeKey AS [EmployeeKey],
+    SalesAmountQuota AS [Sales Amount Quota]
+FROM FactSalesQuota ; 
+>>>>>>> a18143be2e121dacd996d4ec19789bdfe19761b9:Sql-Scripts/01_Gold_Reporting_Views.sql
 GO
 
 -- =============================================================================
@@ -201,26 +258,55 @@ IF OBJECT_ID('v_InternetFactSales', 'V') IS NOT NULL
     DROP VIEW v_InternetFactSales;
 GO
 
-CREATE VIEW v_InternetFactSales AS
+CREATE OR ALTER VIEW v_InternetFactSales AS
 SELECT 
-    f.SalesOrderNumber,
-    f.SalesOrderLineNumber,
-    f.OrderDateKey,
-    f.CustomerKey,
-    f.ProductKey,
-    f.PromotionKey, 
-    f.SalesTerritoryKey,
-    e.EmployeeKey, 
-    f.OrderQuantity,
-    f.UnitPrice,
-    f.DiscountAmount,
+    f.SalesOrderNumber AS [Sales Order Number],
+    f.SalesOrderLineNumber AS [Sales Order Line Number],
+    f.OrderDateKey AS [OrderDateKey],
+    f.CustomerKey AS [CustomerKey],
+    f.ProductKey AS [ProductKey],
+    f.PromotionKey AS [PromotionKey], 
+    f.SalesTerritoryKey AS [SalesTerritoryKey],    
+    f.OrderQuantity AS [Order Quantity],
+    f.UnitPrice AS [Unit Price],
+    f.DiscountAmount AS [Discount Amount],   
     f.SalesAmount AS [Sales Amount],
     f.TotalProductCost AS [Total Cost],
     (f.SalesAmount - f.TotalProductCost) AS [Profit Amount],
-    p.EnglishPromotionName AS [Promotion Name] 
+    p.EnglishPromotionName AS [Promotion Name],
+    
+    ISNULL(r.SalesReasonName, 'Normal Sale') AS [Sales Reason],
+    CASE 
+        WHEN r.SalesReasonReasonType = 'Other' THEN 1 
+        WHEN r.SalesReasonName = 'Price' THEN 1 
+        ELSE 0 
+    END AS [Is Return Flag]
 FROM FactInternetSales f
-LEFT JOIN DimEmployee e 
-    ON f.SalesTerritoryKey = e.SalesTerritoryKey
-LEFT JOIN DimPromotion p
-    ON f.PromotionKey = p.PromotionKey;
+LEFT JOIN DimPromotion p ON f.PromotionKey = p.PromotionKey
+LEFT JOIN FactInternetSalesReason fisr  ON f.SalesOrderNumber = fisr.SalesOrderNumber
+LEFT JOIN DimSalesReason r ON fisr.SalesReasonKey = r.SalesReasonKey;
 GO
+
+
+-- =============================================================================
+-- Create Fact Table: v_FactResellerSales
+-- =============================================================================
+
+IF OBJECT_ID('v_FactResellerSales', 'V') IS NOT NULL
+    DROP VIEW v_FactResellerSales;
+GO
+
+CREATE OR ALTER VIEW v_FactResellerSales AS
+SELECT 
+    SalesOrderNumber AS [Sales Order Number],
+    OrderDateKey AS [OrderDateKey],
+    ResellerKey AS [ResellerKey],
+    ProductKey AS [ProductKey],
+    EmployeeKey AS [EmployeeKey],
+    SalesTerritoryKey AS [SalesTerritoryKey],
+    OrderQuantity AS [Order Quantity],
+    UnitPrice AS [Unit Price],
+    SalesAmount AS [Reseller Sales],
+    TotalProductCost AS [Reseller Cost],
+    (SalesAmount - TotalProductCost) AS [Reseller Profit]
+FROM FactResellerSales;
